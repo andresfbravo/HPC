97ac3024a490b4abed80a0bc4ad13cd2b7febb8f
utp <utp@utp>
2019-04-05 16:00:59 -0500

    arreglar aleatorios, siempre se genera el mismo

--- MultMatrices/matrices.cpp
+++ MultMatrices/matrices.cpp
@@ -1,29 +1,57 @@
 #include <math.h>
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+//https://es.stackoverflow.com/questions/33290/matriz-como-par%C3%A1metro-c
+//https://foro.elhacker.net/programacion_cc/llenar_una_matriz_en_c-t362929.0.html
 
 using namespace std;
 
 int leerDato(){
 	int d;
-	cout<<"ingrese valor: ";
+	cout<<"ingrese tamaño de la matriz: ";
 	cin>>d;
 	return d;
 }
 
-int numeroFilas(){
-	int f;
-	f = leerDato();
-	return f;
+int numeroAleatorio(void){
+    srand(time(NULL));
+    return 1 + rand() % 1000;
 }
 
-int numeroCol(){
-	int c;
-	c = leerDato();
-	return c;
+/*
+int multiplicar(int a, int b){
+	return a*b;
+} */
+
+void llenarMatriz(vector<int>& matriz){
+	for(int i=0; i<matriz.size();i++){
+		matriz[i]=numeroAleatorio();
+	}
+}
+
+void imprimir(vector<int>& matriz, int n){
+	for(int i=0; i<n; i++){
+		for(int j=0;j<n;j++){
+			cout<<matriz[i*n+j]<<"\t";
+		}
+		cout<<endl;
+	}
+}
+
+void multiplicarMatriz(){
+
 }
 
 int main(){
-	int filas = numeroFilas();
-	
-	return 0;
+	int n=leerDato();
+	vector<int> matA(n*n,0),matB(n*n,0);
+	llenarMatriz(matA);
+	llenarMatriz(matB);
+	imprimir(matA,n);
+	//multiplicarMatrices(matA,matB);
+
+
+	//return 0;
 }
