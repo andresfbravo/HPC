a8ad6263a16d8831e5aac664a1775986a01e8286
bravo95 <andresfelipe.bravo@utp.edu.co>
2019-04-08 11:45:06 -0500

    arreglado

--- MultMatrices/matrices.cpp
+++ MultMatrices/matrices.cpp
@@ -16,7 +16,7 @@ int leerDato(){
 }
 
 int numeroAleatorio(void){
-    srand(time(NULL));
+    //srand(time(NULL));
     return 1 + rand() % 1000;
 }
 
@@ -26,6 +26,8 @@ int multiplicar(int a, int b){
 } */
 
 void llenarMatriz(vector<int>& matriz){
+	//srand(time(NULL));
+    
 	for(int i=0; i<matriz.size();i++){
 		matriz[i]=numeroAleatorio();
 	}
@@ -40,17 +42,31 @@ void imprimir(vector<int>& matriz, int n){
 	}
 }
 
-void multiplicarMatriz(){
+void multiplicarMatrices(vector<int>& matA, vector<int>& matB,vector<int>& matC, int n){
+	for (int g=0;g<n*n;g++){
+		int aux;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				
+			}
+		}
+		matC=
+	}
 
 }
 
 int main(){
+	srand(time(NULL));
 	int n=leerDato();
-	vector<int> matA(n*n,0),matB(n*n,0);
+	vector<int> matA(n*n,0),matB(n*n,0),matC(n*n*n,0);
 	llenarMatriz(matA);
 	llenarMatriz(matB);
+	cout<<"matA"<<endl<<endl;
 	imprimir(matA,n);
-	//multiplicarMatrices(matA,matB);
+	cout<<"\nmatB"<<endl<<endl;
+	imprimir(matB,n);
+	
+	multiplicarMatrices(matA,matB,matC,n);
 
 
 	//return 0;
